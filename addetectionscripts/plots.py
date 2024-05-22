282191b357a2175994f50952217c08df5c7fd249
zadealfalah <zadealfalah@gmail.com>
2024-05-22 13:56:00 -0500

    Update gitignore, training scripts

--- addetectionscripts/plots.py
+++ addetectionscripts/plots.py
@@ -1,28 +1,26 @@
 # Plots for mlflow logging
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import xgboost as xgb
 
 def plot_correlation(X: pd.DataFrame, y_df: pd.DataFrame, target_col: str = 'is_attributed',
-                     save_path: str = None, style: str = 'seaborn', plot_size: tuple = (12, 8)):
+                     save_path: str = None, plot_size: tuple = (12, 8)):
     X_df = X.copy()
     # Combine the X and y data again
     X_df[target_col] = y_df[target_col]
     # Get corr values w/ all vars wrt the target
     correlations = X_df.corr()[target_col].drop(target_col).sort_values()
     
-    with plt.style.context(style=style):
-        # Make bar plot
-        fig = plt.figure(figsize=plot_size)
-        plt.barh(correlations.index, correlations.values)
-        
-        # Set Labels
-        plt.title(f'Correlation with {target_col}')
-        plt.xlabel('Correlation Coefficient')
-        plt.ylabel('Variable')
-        plt.grid(axis='x')
-        plt.tight_layout()
+    # Make bar plot
+    fig = plt.figure(figsize=plot_size)
+    plt.barh(correlations.index, correlations.values)
+    
+    # Set Labels
+    plt.title(f'Correlation with {target_col}')
+    plt.xlabel('Correlation Coefficient')
+    plt.ylabel('Variable')
+    plt.grid(axis='x')
+    plt.tight_layout()
         
     if save_path:
         plt.savefig(save_path, format='png', dpi=600)
--- addetectionscripts/transforms.py
+++ addetectionscripts/transforms.py
@@ -20,12 +20,12 @@ def init_datasets(data_folder="datasets", to_load: List[str] = ['X_us', 'y_us', 
     # Read CSV files into pandas DataFrames
     X_us, y_us, test = None, None, None
     if 'X_us' in to_load:
-        X_us = pd.read_csv(f'../{data_folder}/X_us.csv')
+        X_us = pd.read_csv(f'./{data_folder}/X_us.csv')
         X_us['click_time'] = pd.to_datetime(X_us['click_time'])
     if 'y_us' in to_load:
-        y_us = pd.read_csv(f'../{data_folder}/y_us.csv')
+        y_us = pd.read_csv(f'./{data_folder}/y_us.csv')
     if 'test' in to_load:
-        test = pd.read_csv(f'../{data_folder}/test.csv')
+        test = pd.read_csv(f'./{data_folder}/test.csv')
         test['click_time'] = pd.to_datetime(test['click_time'])
     
 
